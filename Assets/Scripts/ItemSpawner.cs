87f04348bd2d7b3ebac176b259982d21b821e71d
CogentInvalid <pchipman@email.arizona.edu>
2016-10-15 11:09:32 -0700

    Changed BalloonSpawner to generic ItemSpawner

---
+++ Assets/Scripts/ItemSpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSpawner : MonoBehaviour {
+
+	public GameObject spawnItem;
+
+	private GameObject spawnedItem;
+
+	public void SpawnItem() {
+		GameObject b = Instantiate(spawnItem) as GameObject;
+		b.transform.position = transform.position + new Vector3(0, 1, 0);
+
+		if (spawnedItem != null) {
+			Destroy(spawnedItem);
+		}
+
+		spawnedItem = b;
+	}
+}
