199a997a2e597a53605962ad557656a07b8df90f
CogentInvalid <pchipman@email.arizona.edu>
2016-10-14 20:07:50 -0700

    Added button

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public Material offMaterial;
+	public Material onMaterial;
+
+	void Update() {
+
+	}
+
+	void OnTriggerStay(Collider other) {
+		if (other.GetComponent<Box>() != null) {
+			SwitchOn();
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		if (other.GetComponent<Box>() != null) {
+			SwitchOff();
+		}
+	}
+
+	void SwitchOn() {
+		gameObject.GetComponent<MeshRenderer>().material = onMaterial;
+	}
+
+	void SwitchOff() {
+		gameObject.GetComponent<MeshRenderer>().material = offMaterial;
+	}
+
+}
