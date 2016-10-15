fa63ff7ccffea062b5b75c58610bf692b1ca9af4
CogentInvalid <pchipman@email.arizona.edu>
2016-10-14 23:16:12 -0700

    Superfluous stuff (exit pt. 1)

---
+++ Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CanvasScript : MonoBehaviour {
+
+	public GameObject white;
+	public bool fadeIn = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color color = white.GetComponent<Image>().color;
+		if (fadeIn) {
+			color.a += Time.deltaTime*0.2f;
+		} else {
+			color.a -= Time.deltaTime;
+		}
+		white.GetComponent<Image>().color = color;
+	}
+}
---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Exit : MonoBehaviour {
+
+	GameObject player;
+
+	float timer = 5;
+
+	// Use this for initialization
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "Player") {
+			player = other.gameObject;
+			player.GetComponent<Rigidbody>().useGravity = false;
+			player.GetComponent<CharacterController>().enabled = false;
+
+			GameObject.Find("Canvas").GetComponent<CanvasScript>().fadeIn = true;
+		}
+	}
+
+	void Update() {
+		if (player != null) {
+			Vector3 pos = player.transform.position;
+			float y = pos.y + 3*Time.deltaTime;
+			Vector3 targetPos = new Vector3(transform.position.x, y, transform.position.z);
+			pos -= (pos-targetPos)*5*Time.deltaTime;
+			pos.y = y;
+			player.transform.position = pos;
+
+			timer -= Time.deltaTime;
+		}
+	}
+}
